34577555b97caf31126acaa042b362df742480d9
Menglii <wydnm060722@163.com>
2024-11-02 14:58:34 +0800

    初步集成了面部蒙版，深度图和边缘图控制网功能

---
+++ demo/demo_11_1/MY4A.py
@@ -0,0 +1,261 @@
+#IMG2IMG图生图程序
+
+import bz2
+import torch
+import requests
+from PIL import Image
+from io import BytesIO
+from matplotlib import pyplot as plt
+import os
+from PIL import Image
+from compel import Compel
+
+from preprocessor import (
+    get_depth_map,
+    get_array_map,
+    create_face_mask,    
+    )
+
+from diffusers import (
+    StableDiffusionPipeline, 
+    StableDiffusionImg2ImgPipeline,
+    StableDiffusionInpaintPipeline, 
+    StableDiffusionDepth2ImgPipeline,
+    DPMSolverMultistepScheduler,
+    ControlNetModel,
+    StableDiffusionControlNetImg2ImgPipeline,
+    StableDiffusionControlNetPipeline,
+    AutoPipelineForInpainting,
+    )       
+          
+
+def test(inuput_picture_address,
+        prompt_in=None,
+        negative_prompt_in=None, 
+        guidance_scale_in=8, 
+        steps_in=35, 
+        strength_in=0.6,
+        seed_in=1, 
+        control_net_kind=None,
+        lora = None,
+        face_mask = None
+        ):
+    
+    
+    device = (
+        "mps"
+        if torch.backends.mps.is_available()
+        else "cuda"
+        if torch.cuda.is_available()
+        else "cpu"
+    )
+    print(device)
+
+    prompt = prompt_in
+    negative_prompt = negative_prompt_in
+    guidance_scale = guidance_scale_in
+    init_image = Image.open(inuput_picture_address)
+    steps = steps_in
+    strength = strength_in
+    seed = seed_in
+    model_id = "./models/models--stable-diffusion-v1-5--stable-diffusion-v1-5"
+    generator = torch.Generator(device=device).manual_seed(seed)
+
+
+
+    #这是一个压缩图片程序，防止我的电脑显存不足报错，用户应该可以选择想要的压缩程度
+    def compress_if_large(init_image, max_size=(720, 720)):
+
+        original_width, original_height = init_image.size
+        # 检查图片是否超过指定尺寸
+        if original_width > max_size[0] or original_height > max_size[1]:   
+            init_image.thumbnail(max_size)                                  #thumbnail是等比压缩方法，包含于从PIL库中import的Image
+            print(f"图片已压缩为: {init_image.size}")
+        else:
+            print("图片尺寸合适，无需压缩。")
+
+    compress_if_large(init_image)
+
+
+
+
+
+    def add_scheduler():
+        pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+
+    def add_lora():
+        lora = 0
+        #Lora
+        #lora = "./Lora_models/fechin.safetensors"
+        #monet_v2-000004.safetensors
+        if lora == 1:
+            lora = "./Lora_models/monet_v2-000004.safetensors"
+            lora_scale = 1.0
+            pipe.load_lora_weights(lora,adapter_name="Lora1")
+            adapter_weight_scales = {
+                "unet":{
+                    "down":0.5,
+                    "mid":0.5,
+                    "up":0.5
+                }             
+                                    }
+
+            pipe.set_adapters("Lora1", adapter_weight_scales)
+
+    #promt预处理，使用户可以在词后加入+或-改变权重
+    def compel_prompt(prompt):   
+        compel_proc = Compel(tokenizer=pipe.tokenizer, text_encoder=pipe.text_encoder)
+        prompt_embeds = compel_proc(prompt)
+        return prompt_embeds    
+
+
+
+
+
+
+    #检测控制网选择类型，构建管道, 生成图片
+    if control_net_kind==None and face_mask==None:
+
+        pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
+            model_id, 
+            revision="fp16",
+            torch_dtype=torch.float16,
+            cache_dir="./models/",
+            use_safetensors=True,
+            ).to(device)
+        
+        add_scheduler()
+        add_lora()
+        prompt_embeds = compel_prompt(prompt)
+
+        pipe_output = pipe(
+        prompt_embeds=prompt_embeds,
+        negative_prompt=negative_prompt,  # 这个也是
+        #height=512, width=512,     # 图片大小，在此处不会生效
+        guidance_scale=guidance_scale,
+        num_inference_steps=steps,
+        generator=generator,
+        strength=strength,
+        image=init_image
+        #cross_attention_kwargs={"scale":lora_scale}
+    )
+        print("1")
+
+    elif control_net_kind==None and face_mask!=None:
+
+
+
+        pipe = AutoPipelineForInpainting.from_pretrained(
+        model_id, 
+        revision="fp16",
+        torch_dtype=torch.float16,
+        cache_dir="./cache/",
+        use_safetensors=True,
+        ).to(device)
+
+        mask = create_face_mask(init_image, face_mask)
+        blurred_mask = pipe.mask_processor.blur(mask, blur_factor=33)
+        
+        add_scheduler()
+        add_lora()
+        prompt_embeds = compel_prompt(prompt)
+
+        pipe_output = pipe(
+        prompt_embeds=prompt_embeds,  
+        negative_prompt=negative_prompt,  
+        #height=512, width=512,     # 图片大小，在此处不会生效
+        guidance_scale=guidance_scale,
+        num_inference_steps=steps,
+        generator=generator,
+        strength=strength,
+        image=init_image,
+        mask_image = blurred_mask,
+        )
+        print("2")
+        
+    elif control_net_kind=="dpt" and face_mask==None:
+            
+            depth_image = get_depth_map(init_image)
+
+            net_model_address = "./ControlNet_models/models--lllyasviel--control_v11f1p_sd15_depth"
+            controlnet = ControlNetModel.from_pretrained(net_model_address, torch_dtype=torch.float16, use_safetensors=True, cache_dir="./ControlNet_models").to(device)
+
+            pipe = StableDiffusionControlNetPipeline.from_pretrained(
+            model_id, 
+            revision="fp16",
+            torch_dtype=torch.float16,
+            cache_dir="./models/",
+            use_safetensors=True,
+            controlnet=controlnet,
+            ).to(device)
+
+            add_scheduler()
+            add_lora()
+            prompt_embeds = compel_prompt(prompt)
+
+            pipe_output = pipe(
+            prompt_embeds=prompt_embeds,  
+            negative_prompt=negative_prompt,  
+            #height=512, width=512,     # 图片大小，在此处不会生效
+            guidance_scale=guidance_scale,
+            num_inference_steps=steps,
+            generator=generator,
+            strength=strength,
+            image=depth_image,
+            controlnet_conditioning_scale=1.0,
+            #control_guidance_start=0.0,
+            #control_guidance_end=1.0,
+            )
+            print("3")
+
+    elif control_net_kind=="array" and face_mask==None:
+        array_image = get_array_map(init_image)
+
+        net_model_address = "./ControlNet_models/models--lllyasviel--control_v11f1p_sd15_depth"
+        controlnet = ControlNetModel.from_pretrained(net_model_address, torch_dtype=torch.float16, use_safetensors=True, cache_dir="./ControlNet_models").to(device)
+
+        pipe = StableDiffusionControlNetPipeline.from_pretrained(
+        model_id, 
+        revision="fp16",
+        torch_dtype=torch.float16,
+        cache_dir="./models/",
+        use_safetensors=True,
+        controlnet=controlnet,
+        ).to(device)
+
+
+        add_scheduler()
+        add_lora()
+        prompt_embeds = compel_prompt(prompt)
+
+        pipe_output = pipe(
+        prompt_embeds=prompt_embeds,  
+        negative_prompt=negative_prompt,  
+        #height=512, width=512,     # 图片大小，在此处不会生效
+        guidance_scale=guidance_scale,
+        num_inference_steps=steps,
+        generator=generator,
+        strength=strength,
+        image=array_image,
+        controlnet_conditioning_scale=1.0,
+        #control_guidance_start=0.0,
+        #control_guidance_end=1.0,
+            )
+        print("4")
+    
+        
+
+    
+
+
+
+
+
+
+
+
+    print(f"----本次测试输出：")
+    print(f"模型文件:{model_id}\nprompt：{prompt}\nnegative_prompt：{negative_prompt}\n指导程度：{guidance_scale}\n采样步数：{steps}\n更改强度：{strength}\n种子：{seed}")
+    #pipe_output.images[0].save("./result_test/A.png")
+    #暂时保存
+    return(pipe_output.images[0])
---
+++ demo/demo_11_1/preprocessor.py
@@ -0,0 +1,89 @@
+import torch
+import numpy as np
+from transformers import pipeline
+import cv2
+from PIL import Image
+import mediapipe as mp
+
+#(1, 3, H, W)torch张量转换为PIL,保存预处理图片
+def tensor_to_pil(depth_map):
+    # 去掉 batch 维度并转换为形状 (H, W, 3)
+    depth_map_np = depth_map.squeeze().permute(1, 2, 0).cpu().numpy()
+    # 将浮点像素值（0-1）转换为 0-255 范围的 uint8 类型
+    depth_map_np = (depth_map_np * 255).astype(np.uint8)
+    # 转换为PIL
+    pil_image = Image.fromarray(depth_map_np)
+    return pil_image
+
+#边缘图
+def get_array_map(init_image):
+    image_array = cv2.cvtColor(np.array(init_image), cv2.COLOR_RGB2GRAY)
+    image_array = np.array(image_array)
+    image_array = cv2.Canny(
+      image_array,
+      100, #canny_lower_threshold,
+      200, #canny_higher_threshold
+    )
+    image_array = image_array[:, :, None]
+    image_array = np.concatenate([image_array, image_array, image_array], axis=2)
+    return image_array
+
+#深度图
+def get_depth_map(init_image):
+    depth_estimator_model = "./ControlNet_models/models--Intel--dpt-beit-large-512"
+    depth_estimator = pipeline("depth-estimation", model=depth_estimator_model, cache_dir="./cache/")
+
+    image = depth_estimator(init_image)["depth"]
+    image = np.array(image)
+    image = image[:, :, None]
+    image = np.concatenate([image, image, image], axis=2)
+    detected_map = torch.from_numpy(image).float() / 255.0
+    depth_map = detected_map.permute(2, 0, 1)
+    depth_map = depth_map.unsqueeze(0).half()
+    test_image = tensor_to_pil(depth_map)
+    test_image.save("./AAA/0A1.png")
+
+    return depth_map
+
+
+#脸部蒙版
+def create_face_mask(pil_image, face_mask_class):
+    # 初始化MediaPipe人脸检测模块
+    mp_face_detection = mp.solutions.face_detection
+    face_detection = mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.3)
+    #1远景，0近景
+    #最低置信度
+
+    # 将PIL图像转换为OpenCV格式
+    image = np.array(pil_image)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    # 检测人脸
+    results = face_detection.process(image_rgb)
+
+    # 创建空白蒙版
+    mask = np.zeros_like(image)
+
+    if results.detections:
+        for detection in results.detections:
+            # 获取人脸边界框
+            bboxC = detection.location_data.relative_bounding_box
+            ih, iw, _ = image.shape
+            x1, y1 = int(bboxC.xmin * iw), int(bboxC.ymin * ih)
+            x2, y2 = int((bboxC.xmin + bboxC.width) * iw), int((bboxC.ymin + bboxC.height) * ih)
+            
+            # 在蒙版上绘制白色矩形表示人脸区域
+            mask[y1:y2, x1:x2] = 255
+
+    if face_mask_class == 0:
+        mask = cv2.bitwise_not(mask)
+    # 将蒙版转换为PIL图像
+    mask_image = Image.fromarray(cv2.cvtColor(mask, cv2.COLOR_BGR2RGB))
+    mask_image.save("./AAA/0A1.png")
+
+
+    # 清理资源
+    face_detection.close()
+
+    return mask_image
+
